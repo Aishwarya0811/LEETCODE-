d01cb044dcf80104e874a202f01d15341a2ddc15
Aishwarya Badlani <41635755+Aishwarya0811@users.noreply.github.com>
2019-09-23 22:32:06 -0400

    Update Remove duplicates from sorted array.py

--- Array/Remove duplicates from sorted array.py
+++ Array/Remove duplicates from sorted array.py
@@ -11,3 +11,14 @@ class Solution:
         nums.clear()
         nums.extend(a)
         return len(nums)
+    
+    
+# Time O(n), Space O(1)   
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[i]
+          return slow + 1
