16f3ae038a54070845b889aada6ea58f5a733170
Aishwarya Badlani <41635755+Aishwarya0811@users.noreply.github.com>
2019-09-23 22:29:02 -0400

    Update Remove duplicates from sorted array.py

--- Array/Remove duplicates from sorted array.py
+++ Array/Remove duplicates from sorted array.py
@@ -1,3 +1,13 @@
 
 ** Given a sorted array nums, remove the duplicates in-place such that 
     each element appear only once and return the new length.**
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        a = list(set(nums))
+        a.sort()
+        nums.clear()
+        nums.extend(a)
+        return len(nums)
